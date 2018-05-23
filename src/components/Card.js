aac1d2f5a5c255b8c2d16a129f58cbc9b99e1d6a
remind <andersonht365@gmail.com>
2018-05-23 09:01:15 -0400

    Positioning Title and text input in same place

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,6 +9,7 @@ class Card extends Component {
       inputDisplay : this.props.title ? false : true, 
       title : this.props.title ? this.props.title : 'Double Click To Add a Title'
     }
+    this.handleTitleState = this.handleTitleState.bind(this);
   }
   
   handleTitleState(){
@@ -18,26 +19,23 @@ class Card extends Component {
   }
 
   getTitle(){
-    return (
-      <div style={{display: !this.state.inputDisplay ? 'none' : 'visible'}}
-        onDoubleClick={this.handleTitleState.bind(this)}>
-        { this.state.title }
-      </div>);
+    return(
+      <div> 
+        { this.state.inputDisplay && <Input placeholder={this.state.title} /> }
+        { !this.state.inputDisplay && <div onDoubleClick={this.handleTitleState}>  { this.state.title } </div> }
+      </div>
+    );
   }
 
   render () {
     let titleDiv = this.getTitle();
-    let displayValue = this.state.inputDisplay ? 'none' : 'visible';
+    
     return(
       <AntCard title={ titleDiv } 
         style={{width : 300 }}> 
           <p>Card content</p>
           <p>Card content</p>
           <p>Card content</p>       
-          <div style={{ display: displayValue }}>
-            Hi
-            <Input placeholder={this.state.title} />
-          </div>
       </AntCard>
   )}
 }
